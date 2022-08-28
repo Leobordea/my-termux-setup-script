76fd38a50d19e9da5ea2fcb97006c6b82d8b01ba
Phuc Hoang <hoangmaithienphuc@gmail.com>
2022-08-28 20:13:23 +0700

    update git

--- git.sh
+++ git.sh
@@ -1,3 +1,5 @@
 git config --global user.name "Phuc Hoang"
 git config --global user.email hoangmaithienphuc@gmail.com
 git config --global core.editor nvim
+git config --global init.defaultBranch main
+git config --global alias.logline "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
